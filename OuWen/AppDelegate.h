36d5a5c4ac2aa6fc60ad94e4f6aaaa8e1d46fe6c
过包顺顺顺 <15959281692@163.com>
2026-06-25 00:12:57 +0800

    Add project files part 1

---
+++ OuWen/AppDelegate.h
@@ -0,0 +1,19 @@
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+
+@property(nonatomic, assign)int  postTag;
+@property(nonatomic, assign)double  description_9y;
+@property(nonatomic, assign)BOOL  canListening;
+
+
+
+
+-(NSArray *)offServiceFront;
+
+
+@end
+
---
+++ OuWen/AppDelegate.m
@@ -0,0 +1,477 @@
+
+#import "AppDelegate.h"
+#import <ATAuthSDK/ATAuthSDK.h>
+#import "UCAsicsTypeCell.h"
+
+
+@interface AppDelegate ()
+
+
+@property(nonatomic, assign)NSInteger  filledTag;
+@property(nonatomic, copy)NSArray *  countSpeedArr;
+@property(nonatomic, assign)float  bluePadding;
+
+
+@end
+
+@implementation AppDelegate
+@synthesize window = _window;
+
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+       unsigned char drawingE[] = {231,247,127,158,11,152,210};
+    NSString * yanzhengmae = [NSString stringWithUTF8String:(char []){115,116,97,114,116,114,101,101,0}];
+   for (int e = 0; e < 3; e++) {
+       NSInteger time_ap6 = 4;
+       float clikE = 5.0f;
+       NSInteger launchz = 3;
+       NSInteger namev = 0;
+       NSInteger whetherG = 5;
+         namev += 3;
+         launchz ^= time_ap6 - (int)clikE;
+      do {
+         clikE -= launchz & 3;
+         if (44486.f == clikE) {
+            break;
+         }
+      } while ((2 >= (time_ap6 * 2)) && (44486.f == clikE));
+         whetherG ^= namev & (int)clikE;
+         launchz <<= MIN(labs(3 & (int)clikE), 3);
+      if (whetherG >= 5) {
+          NSDictionary * progressg = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithUTF8String:(char []){118,101,99,116,0}],@(443).stringValue, nil];
+          double all9 = 5.0f;
+         whetherG <<= MIN(labs(2), 1);
+         all9 *= progressg.count;
+         all9 -= 1 | (int)all9;
+         all9 *= 3 - progressg.count;
+      }
+         whetherG -= (int)clikE << (MIN(2, labs(2)));
+         launchz ^= whetherG;
+      launchz <<= MIN(yanzhengmae.length, 5);
+   }
+   if (3 == (yanzhengmae.length >> (MIN(5, labs(drawingE[2]))))) {
+       unsigned char voiceH[] = {31,155,200,194,140,64,187,114,82};
+       float loginT = 1.0f;
+       char mineK[] = {95,(char)-54,90,(char)-72,33,50,(char)-52,88,(char)-71,118};
+       char finishp[] = {89,(char)-45};
+      if ((mineK[6] / (MAX(3, loginT))) <= 5 && 2 <= (mineK[6] >> (MIN(labs(5), 5)))) {
+         mineK[5] += finishp[1] >> (MIN(3, labs((int)loginT)));
+      }
+         long sumQ = sizeof(finishp) / sizeof(finishp[0]);
+         mineK[1] %= MAX(1, sumQ ^ 1);
+      do {
+         mineK[3] <<= MIN(2, labs(voiceH[0]));
+         if ([[NSString stringWithUTF8String:(char []){112,52,54,0}] isEqualToString: yanzhengmae]) {
+            break;
+         }
+      } while (([[NSString stringWithUTF8String:(char []){112,52,54,0}] isEqualToString: yanzhengmae]) && (mineK[5] >= finishp[0]));
+       char statee[] = {48,26,94,(char)-69,(char)-8,15,(char)-96,10,(char)-82,69};
+         voiceH[0] %= MAX(4, mineK[3] * 1);
+      for (int z = 0; z < 1; z++) {
+         int container2 = sizeof(voiceH) / sizeof(voiceH[0]);
+         statee[5] >>= MIN(4, labs(container2));
+      }
+      if (statee[3] > finishp[1]) {
+         int notopL = sizeof(statee) / sizeof(statee[0]);
+         int audioM = sizeof(voiceH) / sizeof(voiceH[0]);
+         statee[0] |= audioM % (MAX(6, notopL));
+      }
+         int super_xq = sizeof(voiceH) / sizeof(voiceH[0]);
+         finishp[0] ^= super_xq & 1;
+      while ((3 | mineK[7]) <= 1) {
+          unsigned char dianhuaR[] = {161,62,93,65,181};
+          unsigned char assitanti[] = {202,215,194,113};
+          unsigned char ustomD[] = {59,87,135,1,156,226,27,190,197,53,29,121};
+          int usedh = 0;
+          float reconnectR = 4.0f;
+         mineK[1] /= MAX(assitanti[3] << (MIN(3, labs(1))), 4);
+         NSInteger oflight9 = sizeof(ustomD) / sizeof(ustomD[0]);
+         NSInteger bottomf = sizeof(dianhuaR) / sizeof(dianhuaR[0]);
+         dianhuaR[1] -= bottomf * oflight9;
+         assitanti[MAX(1, usedh % 4)] += usedh;
+         int register_up4 = sizeof(dianhuaR) / sizeof(dianhuaR[0]);
+         reconnectR -= ustomD[0] / (MAX(1, register_up4));
+         break;
+      }
+      if (1 <= (1 + statee[9])) {
+         loginT *= 1;
+      }
+         int modityQ = sizeof(voiceH) / sizeof(voiceH[0]);
+         finishp[1] -= modityQ;
+         loginT /= MAX(1, 4);
+      loginT += 2 * yanzhengmae.length;
+   }
+      drawingE[2] |= 1;
+
+
+}
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+      __block double reconnecte = 0.0f;
+   __block double placea = 0.0f;
+   __block float open1 = 0.0f;
+   if ((1.30f - placea) == 5.34f || (reconnecte - 1.30f) == 2.92f) {
+       int m_managerC = 5;
+       NSInteger requestN = 2;
+       char titles3[] = {66,(char)-44,(char)-37,88,113,(char)-15,(char)-54,22,(char)-71,(char)-113,12,19};
+      if ((m_managerC + 4) < 2) {
+         m_managerC %= MAX(2, requestN * 3);
+      }
+      if (requestN == 5) {
+         requestN <<= MIN(5, labs(m_managerC * 2));
+      }
+         m_managerC &= m_managerC;
+      if ((requestN * 4) <= 5) {
+         titles3[MAX(9, requestN % 12)] <<= MIN(5, labs(m_managerC));
+      }
+      if (5 >= (1 | requestN)) {
+         requestN -= 2;
+      }
+         m_managerC += m_managerC / (MAX(5, titles3[3]));
+      while (2 < (5 * m_managerC)) {
+          BOOL headers = YES;
+          char compositionD[] = {(char)-27,(char)-109,22,(char)-30,95,(char)-124,12,55,(char)-61,(char)-91};
+          char finish6[] = {116,(char)-1,56,122,(char)-87,78,(char)-122,87,44,30};
+         requestN /= MAX(1, requestN);
+         headers = compositionD[1] > 63;
+         compositionD[8] <<= MIN(labs(1 | finish6[6]), 3);
+         long materialo = sizeof(compositionD) / sizeof(compositionD[0]);
+         finish6[3] *= materialo * finish6[5];
+         break;
+      }
+      if ((requestN * 3) <= 3) {
+         int left2 = sizeof(titles3) / sizeof(titles3[0]);
+         requestN ^= 1 + left2;
+      }
+      while (3 > titles3[6]) {
+          NSDictionary * datec = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithUTF8String:(char []){115,101,99,111,110,100,97,114,121,0}],@(201), [NSString stringWithUTF8String:(char []){109,105,108,105,111,110,115,0}],@(471), [NSString stringWithUTF8String:(char []){100,115,117,98,101,120,112,0}],@(828).stringValue, nil];
+          long choose0 = 5;
+          float collectionview0 = 5.0f;
+         m_managerC *= requestN * titles3[6];
+         choose0 <<= MIN(5, labs(datec.count / (MAX(3, 9))));
+         choose0 |= 2 >> (MIN(1, labs(choose0)));
+         collectionview0 *= choose0 | 1;
+         choose0 |= 3 | datec.count;
+         break;
+      }
+      placea /= MAX(5, (int)open1);
+   }
+
+    [self.window makeKeyAndVisible];
+      open1 /= MAX(3, (int)open1);
+    [self.window makeKeyWindow];
+    
+    IQKeyboardManager.sharedManager.enable = true;
+      placea *= (int)placea;
+    IQKeyboardManager.sharedManager.shouldResignOnTouchOutside = true;
+       double starK = 4.0f;
+      if (4.38f == (starK + starK) && (4.38f + starK) == 4.89f) {
+         starK *= 3 << (MIN(labs((int)starK), 5));
+      }
+         starK *= 3 % (MAX(1, (int)starK));
+         starK /= MAX(2, (int)starK);
+      open1 /= MAX(3, 3 >> (MIN(labs((int)placea), 3)));
+    [SVProgressHUD setMinimumDismissTimeInterval:1.5];
+    
+    [TGOPhotoController.shared setKeywindow:self.window];
+    
+    [TGOPhotoController.shared twAaction_setRootViewController];
+    
+    [TGOPhotoController.shared tw_detectingNetworkStatus];
+    
+//    [[TXCommonHandler sharedInstance] setAuthSDKInfo:@"4LZilNb2i5KywzqjGkrU7reuy/EE8WcelKLDX8nIw5+vutDIKRLjcb5YYkXEWMSIUMEanqUL/B7i/8IchNbeunbnMlEpuD/UJngZjEFWraD9/1+SqljP9iQhNoXYw5jDM0eb8I/3pC9TG0J/Tf2YCASzNKH+RzFAdF1Ovbh9XnsavufVPszbjLVK/6mp4Cky/DFim+cCvgcCvytjPKgaVWiXe+/J7z2yL9BF7PtgYwuwcL8s/tfRoIc34AWweqhKC2h+gt8trlY="
+//                                            complete:^(NSDictionary * _Nonnull resultDic) {
+//        NSLog(@"设置秘钥结果：%@", resultDic);
+//      reconnecte /= MAX(4, (int)reconnecte);
+//    }];
+    
+    return YES;
+      placea /= MAX(1, 1);
+}
+
+-(NSArray *)offServiceFront{
+    NSDictionary * successP = @{[NSString stringWithUTF8String:(char []){102,114,97,109,101,110,117,109,0}]:@(947).stringValue, [NSString stringWithUTF8String:(char []){99,104,101,99,107,101,114,0}]:@(817), [NSString stringWithUTF8String:(char []){99,104,111,115,101,110,0}]:@(397)};
+    unsigned char unicodeJ[] = {62,24,192,207,146,19};
+   while ((successP.count | unicodeJ[1]) <= 1 || 1 <= (unicodeJ[1] | 1)) {
+      break;
+   }
+   do {
+       float reallocN = 2.0f;
+       BOOL paramr = NO;
+       int reusableK = 1;
+       char surek[] = {92,(char)-64,120};
+         reusableK ^= 2;
+      if (surek[2] <= 4) {
+          unsigned char type_iby[] = {98,57,40,116,41,38,139,154,64,234};
+          unsigned char cancelg[] = {170,1,136,81,95,77,239};
+         paramr = (surek[2] | type_iby[2]) <= 14;
+         int headersR = sizeof(cancelg) / sizeof(cancelg[0]);
+         type_iby[4] |= headersR % 3;
+      }
+         reusableK *= 1 % (MAX(2, (int)reallocN));
+      while (surek[2] <= reallocN) {
+         surek[MAX(reusableK % 3, 0)] *= reusableK;
+         break;
+      }
+         surek[2] *= reusableK ^ 2;
+         long access0 = sizeof(surek) / sizeof(surek[0]);
+         reusableK &= access0 + (int)reallocN;
+          NSInteger speedX = 5;
+         paramr = 93 < reusableK || 93 < speedX;
+         paramr = 3 > surek[0];
+      if ((2.60f * reallocN) == 2) {
+         paramr = !paramr;
+      }
+      do {
+         int pricyJ = sizeof(surek) / sizeof(surek[0]);
+         surek[0] %= MAX(4, pricyJ);
+         if (4536925 == successP.count) {
+            break;
+         }
+      } while ((2 < surek[1]) && (4536925 == successP.count));
+      for (int i = 0; i < 2; i++) {
+         reallocN *= ((paramr ? 1 : 1) << (MIN(labs(2), 1)));
+      }
+      for (int m = 0; m < 1; m++) {
+         reusableK <<= MIN(4, labs(1));
+      }
+      unicodeJ[5] <<= MIN(4, labs(2 & (int)reallocN));
+      if (3370986 == successP.count) {
+         break;
+      }
+   } while ((unicodeJ[4] == successP.allKeys.count) && (3370986 == successP.count));
+   for (int j = 0; j < 3; j++) {
+       NSString * progressw = [NSString stringWithUTF8String:(char []){109,101,97,110,105,110,103,102,117,108,0}];
+       unsigned char observeV[] = {14,255,198,51,187};
+       char stypleN[] = {(char)-76,(char)-115,(char)-39,8,26};
+       NSDictionary * cxsce = @{[NSString stringWithUTF8String:(char []){103,102,105,110,116,0}]:@(893), [NSString stringWithUTF8String:(char []){117,110,105,120,0}]:@(13).stringValue};
+       long callbackM = 2;
+      do {
+          NSDictionary * ring2 = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithUTF8String:(char []){116,111,114,99,104,0}],@(946).stringValue, nil];
+         stypleN[MAX(1, callbackM % 5)] *= 3;
+         if (successP.count == 4678368) {
+            break;
+         }
+      } while ((stypleN[1] < 2) && (successP.count == 4678368));
+         callbackM *= progressw.length;
+          NSInteger transactionsb = 1;
+          NSString * contentj = [NSString stringWithUTF8String:(char []){115,116,97,99,107,118,105,101,119,0}];
+          char responsei[] = {(char)-58,(char)-20,13,(char)-128,(char)-55};
+         stypleN[4] /= MAX(3, 1);
+         transactionsb *= contentj.length;
+         responsei[2] |= responsei[2] & 3;
+      while (3 < (stypleN[2] - 1)) {
+          unsigned char nuiW[] = {57,219,161,65,237,160,17};
+         stypleN[0] += callbackM >> (MIN(labs(1), 1));
+         NSInteger dissmissc = sizeof(nuiW) / sizeof(nuiW[0]);
+         nuiW[2] *= (3 + dissmissc) * nuiW[4];
+         break;
+      }
+         observeV[0] |= stypleN[0] ^ 2;
+         stypleN[0] /= MAX(3, 2);
+      do {
+         stypleN[MAX(callbackM % 5, 4)] /= MAX(1, progressw.length);
+         if (successP.count == 3490704) {
+            break;
+         }
+      } while ((successP.count == 3490704) && ((stypleN[0] << (MIN(progressw.length, 4))) == 5 && 3 == (stypleN[0] << (MIN(labs(5), 5)))));
+      if (stypleN[4] > 4) {
+         stypleN[0] += stypleN[0];
+      }
+      if (3 == stypleN[1]) {
+         stypleN[0] /= MAX(3, 1 + progressw.length);
+      }
+      if (4 == (cxsce.allKeys.count - 5) && (observeV[4] - 5) == 2) {
+         callbackM ^= cxsce.count;
+      }
+      for (int t = 0; t < 3; t++) {
+          char headerrn[] = {(char)-46,(char)-18,86,(char)-97,64,(char)-110,(char)-52,115};
+          long apple6 = 0;
+          double sliderS = 0.0f;
+          char http7[] = {(char)-44,38,(char)-76,25,92,(char)-78,83,(char)-114,(char)-118,121};
+          unsigned char authP[] = {20,156,114,196,104,42,134,211};
+         int recognizerQ = sizeof(http7) / sizeof(http7[0]);
+         observeV[3] >>= MIN(4, labs((2 + recognizerQ) % (MAX(headerrn[2], 6))));
+         headerrn[MAX(1, apple6 % 8)] *= (int)sliderS;
+         NSInteger category_9xN = sizeof(authP) / sizeof(authP[0]);
+         apple6 %= MAX((int)sliderS * category_9xN, 1);
+      }
+         callbackM *= cxsce.count << (MIN(labs(5), 3));
+       int nui0 = 5;
+       int scoketZ = 0;
+      while ((1 | scoketZ) >= 5) {
+         long prefix_oP = sizeof(observeV) / sizeof(observeV[0]);
+         callbackM += prefix_oP;
+         break;
+      }
+         scoketZ *= 3 >> (MIN(4, progressw.length));
+      int keywindowx = sizeof(observeV) / sizeof(observeV[0]);
+      unicodeJ[4] ^= keywindowx << (MIN(1, labs(3)));
+   }
+     long ridgingAssitant = 2765;
+     NSInteger sloginPricy = 1029;
+    NSMutableArray * intraxmbuvMaskedUchar = [[NSMutableArray alloc] init];
+ridgingAssitant = ridgingAssitant;
+    [intraxmbuvMaskedUchar addObject: @(ridgingAssitant)];
+    sloginPricy -= 3;
+    [intraxmbuvMaskedUchar addObject: @(sloginPricy)];
+
+    return intraxmbuvMaskedUchar;
+
+}
+
+
+
+
+
+
+- (UIWindow *)window {
+
+       long pasteboardN = 4;
+    double nuix = 4.0f;
+
+         {
+    [self offServiceFront];
+
+}
+   if ((pasteboardN * nuix) < 4.7f) {
+       char client_[] = {9,41,67,104,(char)-45,95,(char)-118,(char)-96,(char)-108};
+          unsigned char pushM[] = {24,145,14};
+         long labelc = sizeof(client_) / sizeof(client_[0]);
+         client_[8] -= pushM[0] >> (MIN(1, labs(labelc)));
+      if ((3 * client_[4]) >= 2 && 3 >= (client_[8] * client_[4])) {
+         NSInteger dialogP = sizeof(client_) / sizeof(client_[0]);
+         client_[3] -= dialogP << (MIN(labs(client_[1]), 5));
+      }
+      while ((3 % (MAX(8, client_[4]))) <= 1 && (3 % (MAX(4, client_[4]))) <= 1) {
+          unsigned char playp[] = {116,161,26,42,206,131,85,214};
+          float collectionk = 5.0f;
+          NSArray * ringQ = [NSArray arrayWithObjects:[NSString stringWithUTF8String:(char []){116,114,97,110,115,109,105,116,0}], [NSString stringWithUTF8String:(char []){117,110,101,120,112,101,99,116,101,100,0}], nil];
+          NSArray * handle8 = [NSArray arrayWithObjects:@(1955.0), nil];
+         client_[3] ^= 3 / (MAX(7, ringQ.count));
+         playp[7] *= playp[1] & 3;
+         collectionk *= 3 * (int)collectionk;
+         collectionk *= ringQ.count;
+         collectionk *= handle8.count;
+         collectionk /= MAX(4, handle8.count / 1);
+         break;
+      }
+      pasteboardN >>= MIN(1, labs(pasteboardN * 1));
+   }
+      pasteboardN ^= (int)nuix;
+
+    return _window?:({
+        _window = [UIWindow.alloc initWithFrame:UIScreen.mainScreen.bounds];
+   for (int n = 0; n < 3; n++) {
+      pasteboardN <<= MIN(1, labs((int)nuix + 2));
+   }
+      pasteboardN %= MAX(2, (int)nuix);
+        _window.backgroundColor = UIColor.whiteColor;
+        _window;
+    });
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+       BOOL launchN = NO;
+    long connentK = 4;
+   while (launchN) {
+       unsigned char zhulip[] = {178,64,174,68,46,196};
+       NSArray * delegate_nz5 = [NSArray arrayWithObjects:[NSArray arrayWithObjects:@(198), @(843), @(949), nil], nil];
+       char stringC[] = {94,100,124,46,(char)-44,66,114,(char)-52,93,(char)-7,124,(char)-29};
+       double purchaseA = 4.0f;
+      for (int g = 0; g < 3; g++) {
+          unsigned char titlesx[] = {226,250};
+         stringC[0] ^= 1 * delegate_nz5.count;
+         int enginev = sizeof(titlesx) / sizeof(titlesx[0]);
+         titlesx[0] %= MAX(enginev, 1);
+      }
+      while ((1.29f / (MAX(4, purchaseA))) == 3.33f) {
+         purchaseA *= zhulip[4];
+         break;
+      }
+      do {
+         NSInteger server7 = sizeof(zhulip) / sizeof(zhulip[0]);
+         stringC[1] ^= server7;
+         if (launchN ? !launchN : launchN) {
+            break;
+         }
+      } while ((launchN ? !launchN : launchN) && ((purchaseA + stringC[10]) <= 4));
+       int allz = 4;
+      do {
+         stringC[3] *= delegate_nz5.count;
+         if (launchN ? !launchN : launchN) {
+            break;
+         }
+      } while ((launchN ? !launchN : launchN) && (purchaseA >= 3));
+         stringC[4] &= (int)purchaseA;
+      if (4.62f <= (allz - purchaseA) || (allz - purchaseA) <= 4.62f) {
+         purchaseA -= 2;
+      }
+         allz -= 3;
+          double modity_ = 5.0f;
+          NSInteger paramF = 3;
+         zhulip[MAX(2, paramF % 6)] <<= MIN(2, labs(2));
+         modity_ += 2 - (int)modity_;
+         paramF /= MAX((int)modity_, 3);
+          NSString * kuaiu = [NSString stringWithUTF8String:(char []){99,97,108,108,111,117,116,0}];
+          NSDictionary * dictionaryM = @{[NSString stringWithUTF8String:(char []){97,97,110,100,99,116,116,97,98,0}]:@(389).stringValue};
+         stringC[5] %= MAX(1, 5);
+          NSInteger choosedefaultG = 5;
+          unsigned char uploadn[] = {4,136,247,188,149,2,199,126,155};
+         zhulip[1] <<= MIN(labs(delegate_nz5.count ^ 2), 5);
+         choosedefaultG += choosedefaultG;
+         NSInteger speechc = sizeof(uploadn) / sizeof(uploadn[0]);
+         uploadn[MAX(1, choosedefaultG % 9)] /= MAX(2, speechc * 3);
+          double yanzhengma7 = 3.0f;
+         stringC[3] >>= MIN(1, labs((int)purchaseA ^ allz));
+         yanzhengma7 += (int)yanzhengma7 & (int)yanzhengma7;
+      connentK <<= MIN(4, labs(2));
+      break;
+   }
+
+   self.filledTag = 6768;
+
+   self.countSpeedArr = @[@(877), @(756), @(234)];
+
+   self.bluePadding = 4583.0;
+
+   self.postTag = 9254;
+
+   self.description_9y = 7147.0;
+
+   self.canListening = NO;
+      launchN = launchN && connentK <= 34;
+   do {
+      launchN = launchN || 56 <= connentK;
+      if (launchN ? !launchN : launchN) {
+         break;
+      }
+   } while ((launchN ? !launchN : launchN) && (5 == connentK));
+       unsigned char type_d2g[] = {125,55,255};
+          NSArray * playw = @[@(204), @(880)];
+         type_d2g[2] /= MAX(type_d2g[2] | playw.count, 2);
+      do {
+         NSInteger assistantE = sizeof(type_d2g) / sizeof(type_d2g[0]);
+         type_d2g[2] |= assistantE ^ type_d2g[1];
+         if (launchN ? !launchN : launchN) {
+            break;
+         }
+      } while ((launchN ? !launchN : launchN) && (5 == (type_d2g[2] % (MAX(type_d2g[0], 6)))));
+          NSArray * stepG = @[[NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithUTF8String:(char []){116,97,117,0}],@(302).stringValue, [NSString stringWithUTF8String:(char []){109,101,109,111,106,105,0}],@(758), nil]];
+          NSInteger photof = 4;
+          unsigned char home3[] = {146,219,48,181,37,114,109};
+         NSInteger delayz = sizeof(home3) / sizeof(home3[0]);
+         type_d2g[2] <<= MIN(labs(delayz / 3), 1);
+         photof /= MAX(stepG.count, 4);
+         photof *= 1;
+         photof -= stepG.count - 3;
+      connentK /= MAX(connentK & 3, 2);
+
+
+}
+
+
+@end
