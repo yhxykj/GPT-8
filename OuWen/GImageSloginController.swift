8e8583c7835ea80afad0aabd7d3b365e19123763
过包顺顺顺 <15959281692@163.com>
2026-06-25 00:54:01 +0800

    Add project files part 19

---
+++ OuWen/GImageSloginController.swift
@@ -0,0 +1,189 @@
+
+import Foundation
+
+import UIKit
+import MarkdownKit
+
+class GImageSloginController: UIViewController {
+private var playDisposeString: String?
+var items_offset: Float = 0.0
+
+@discardableResult
+ func shareStrongIncludeImageView(lijiCollectionview: Double, httpPopup: [String: Any]!) -> UIImageView! {
+    var fived: Bool = false
+    var qmui_: Float = 3.0
+      fived = fived && 18.29 < qmui_
+       var window_vcH: [String: Any]! = [String(cString: [115,116,97,98,105,108,105,116,121,0], encoding: .utf8)!:1303.0]
+       var userdataJ: String! = String(cString: [114,101,99,111,110,102,105,103,117,114,97,98,108,101,0], encoding: .utf8)!
+      while (1 >= (window_vcH.values.count * 3) || (window_vcH.values.count * userdataJ.count) >= 3) {
+         window_vcH[userdataJ] = 1 ^ window_vcH.values.count
+         break
+      }
+      if !userdataJ.contains("\(window_vcH.keys.count)") {
+         userdataJ.append("\(2)")
+      }
+         window_vcH[userdataJ] = 2
+      if !userdataJ.hasPrefix("\(window_vcH.values.count)") {
+         userdataJ = "\(userdataJ.count)"
+      }
+      while (userdataJ.hasSuffix("\(window_vcH.count)")) {
+         window_vcH = ["\(window_vcH.values.count)": (userdataJ == (String(cString:[116,0], encoding: .utf8)!) ? window_vcH.values.count : userdataJ.count)]
+         break
+      }
+      repeat {
+          var titlesc: Bool = true
+          var photox: String! = String(cString: [119,111,114,100,115,0], encoding: .utf8)!
+          var speechF: String! = String(cString: [111,116,104,0], encoding: .utf8)!
+         userdataJ = "\(userdataJ.count + speechF.count)"
+         titlesc = (((!titlesc ? photox.count : 60) & photox.count) < 60)
+         speechF.append("\(((String(cString:[80,0], encoding: .utf8)!) == photox ? (titlesc ? 4 : 4) : photox.count))")
+         if userdataJ.count == 1544724 {
+            break
+         }
+      } while (window_vcH.values.count == userdataJ.count) && (userdataJ.count == 1544724)
+      fived = 27.51 < qmui_ || !fived
+      qmui_ /= KAToolRingTool10.max(Float(2 - Int(qmui_)), 3)
+     var recorderClient: UIButton! = UIButton(frame:CGRect(x: 20, y: 61, width: 0, height: 0))
+     let frame_tnSynthesis: Double = 6310.0
+     var mainTool: UIView! = UIView()
+    var pushbackRecursiveUnmanaged: UIImageView! = UIImageView(frame:CGRect(x: 290, y: 375, width: 0, height: 0))
+    recorderClient.frame = CGRect(x: 21, y: 32, width: 0, height: 0)
+    recorderClient.alpha = 0.4;
+    recorderClient.backgroundColor = UIColor(red:0, green:0, blue:0, alpha: 0)
+    
+    var recorderClientFrame = recorderClient.frame
+    recorderClientFrame.size = CGSize(width: 242, height: 96)
+    recorderClient.frame = recorderClientFrame
+    if recorderClient.alpha > 0.0 {
+         recorderClient.alpha = 0.0
+    }
+    if recorderClient.isHidden {
+         recorderClient.isHidden = false
+    }
+    if !recorderClient.isUserInteractionEnabled {
+         recorderClient.isUserInteractionEnabled = true
+    }
+
+    mainTool.frame = CGRect(x: 162, y: 37, width: 0, height: 0)
+    mainTool.alpha = 0.9;
+    mainTool.backgroundColor = UIColor(red:0, green:0, blue:0, alpha: 0)
+    
+    var mainToolFrame = mainTool.frame
+    mainToolFrame.size = CGSize(width: 112, height: 230)
+    mainTool.frame = mainToolFrame
+    if mainTool.alpha > 0.0 {
+         mainTool.alpha = 0.0
+    }
+    if mainTool.isHidden {
+         mainTool.isHidden = false
+    }
+    if !mainTool.isUserInteractionEnabled {
+         mainTool.isUserInteractionEnabled = true
+    }
+
+    pushbackRecursiveUnmanaged.animationRepeatCount = 9
+    pushbackRecursiveUnmanaged.image = UIImage(named:String(cString: [111,114,100,101,114,0], encoding: .utf8)!)
+    pushbackRecursiveUnmanaged.contentMode = .scaleAspectFit
+    pushbackRecursiveUnmanaged.alpha = 0.5;
+    pushbackRecursiveUnmanaged.backgroundColor = UIColor(red:0, green:0, blue:0, alpha: 0)
+    pushbackRecursiveUnmanaged.frame = CGRect(x: 248, y: 238, width: 0, height: 0)
+
+    
+    var pushbackRecursiveUnmanagedFrame = pushbackRecursiveUnmanaged.frame
+    pushbackRecursiveUnmanagedFrame.size = CGSize(width: 212, height: 76)
+    pushbackRecursiveUnmanaged.frame = pushbackRecursiveUnmanagedFrame
+    if pushbackRecursiveUnmanaged.alpha > 0.0 {
+         pushbackRecursiveUnmanaged.alpha = 0.0
+    }
+    if pushbackRecursiveUnmanaged.isHidden {
+         pushbackRecursiveUnmanaged.isHidden = false
+    }
+    if !pushbackRecursiveUnmanaged.isUserInteractionEnabled {
+         pushbackRecursiveUnmanaged.isUserInteractionEnabled = true
+    }
+
+    return pushbackRecursiveUnmanaged
+
+}
+
+
+
+
+
+
+
+
+
+    override func viewDidLoad() {
+
+         let invitesBrace: UIImageView! = shareStrongIncludeImageView(lijiCollectionview:4221.0, httpPopup:[String(cString: [114,116,99,115,116,97,116,115,0], encoding: .utf8)!:799, String(cString: [99,104,105,108,100,114,101,110,0], encoding: .utf8)!:95, String(cString: [115,101,103,0], encoding: .utf8)!:795])
+
+      if invitesBrace != nil {
+          self.view.addSubview(invitesBrace)
+          let invitesBrace_tag = invitesBrace.tag
+      }
+
+
+
+       var peecho: String! = String(cString: [99,111,110,115,101,110,116,0], encoding: .utf8)!
+    var recordQ: String! = String(cString: [114,97,105,110,0], encoding: .utf8)!
+    var oicep: Double = 1.0
+      recordQ.append("\(((String(cString:[71,0], encoding: .utf8)!) == recordQ ? recordQ.count : peecho.count))")
+      oicep *= Double(Int(oicep) - recordQ.count)
+
+   if 2 <= (5 >> (KAToolRingTool10.min(2, recordQ.count))) || (Int(oicep) + recordQ.count) <= 5 {
+       var start5: Double = 5.0
+       var materialf: String! = String(cString: [119,97,107,101,0], encoding: .utf8)!
+       var topF: Double = 5.0
+      while ((materialf.count / (KAToolRingTool10.max(2, 3))) == 5 && (topF + 3.2) == 1.74) {
+         materialf.append("\(materialf.count)")
+         break
+      }
+         materialf.append("\(Int(start5) | materialf.count)")
+          var number6: Bool = true
+          var hengw: String! = String(cString: [117,110,105,119,103,116,0], encoding: .utf8)!
+          var rootU: [String: Any]! = [String(cString: [102,119,97,108,115,104,0], encoding: .utf8)!:614, String(cString: [108,97,116,101,115,116,0], encoding: .utf8)!:54]
+         start5 *= Double(hengw.count ^ materialf.count)
+         number6 = rootU.values.count <= 29
+         hengw.append("\(rootU.values.count + 3)")
+         materialf = "\(3 << (KAToolRingTool10.min(labs(Int(start5)), 5)))"
+         materialf = "\(Int(topF) * Int(start5))"
+         materialf.append("\(Int(start5))")
+       var topM: Bool = true
+       var closeB: Bool = false
+      repeat {
+         topF *= Double(2 % (KAToolRingTool10.max(Int(topF), 4)))
+         if topF == 3243655.0 {
+            break
+         }
+      } while (topF == 3243655.0) && (topM && 5.38 > (topF - 5.5))
+         materialf.append("\(((closeB ? 5 : 1) >> (KAToolRingTool10.min(materialf.count, 5))))")
+      oicep -= Double(2)
+   }
+   repeat {
+      peecho = "\((peecho == (String(cString:[73,0], encoding: .utf8)!) ? peecho.count : peecho.count))"
+      if (String(cString:[56,105,50,54,122,121,53,103,49,0], encoding: .utf8)!) == peecho {
+         break
+      }
+   } while ((String(cString:[56,105,50,54,122,121,53,103,49,0], encoding: .utf8)!) == peecho) && (peecho != peecho)
+        super.viewDidLoad()
+
+        
+        
+      peecho = "\(peecho.count % 1)"
+    }
+    
+
+    
+
+}
+
+ class KAToolRingTool10 {
+    static func min<V: Comparable>(_ p1: V, _ p2: V) -> V {
+        return p1 > p2 ? p2 : p1
+    }
+    
+    static func max<V: Comparable>(_ p1: V, _ p2: V) -> V {
+        return p1 > p2 ? p1 : p2
+    }
+}
---
+++ OuWen/OuKContr/OuKAssi/CNLScreenLazaController.h
@@ -0,0 +1,36 @@
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CNLScreenLazaController : UIViewController
+
+
+@property(nonatomic, assign)NSInteger  labels_mark;
+@property(nonatomic, assign)double  records_space;
+@property(nonatomic, assign)NSInteger  tool_mark;
+@property(nonatomic, assign)double  trad_max;
+
+
+
+
+-(double)hitElementTransactionJian:(NSArray *)timerTitlt objReusable:(NSString *)objReusable firstPopup:(NSArray *)firstPopup;
+
+-(NSDictionary *)weixinStampWillEnabledChuangUtils;
+
+-(long)globalCustomAffineDirecton;
+
+-(BOOL)setupSubstringPstate:(long)controlUtils barrageLobal:(long)barrageLobal completedVoices:(NSString *)completedVoices;
+
+-(double)anchorInterfaceMaskedPrintedPrameterFull;
+
+-(UILabel *)firstPalFragmentDidselectLabel:(NSArray *)greenOut speechAll:(BOOL)speechAll huangAudio:(NSDictionary *)huangAudio;
+
+-(NSArray *)recoverItemClickTail:(long)pasteboardScoket;
+
+
+- (void)getCreateTablelist;
+
+@end
+
+NS_ASSUME_NONNULL_END
